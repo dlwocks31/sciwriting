a272ab94c716ea8e38a384bd2649204b1c6b4bdd
dlwocks <frank_smic@hotmail.com>
2018-05-10 00:58:11 +0900

    htmls created

--- experiment/views.py
+++ experiment/views.py
@@ -1,6 +1,16 @@
+from random import randint
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 
 def index(request):
-    return HttpResponse('파기글주거')
-# Create your views here.
+    return render(request, 'experiment/index.html')
+
+def main(request):
+    mode = randint(1, 2)
+    return render(request, 'experiment/main%d.html' % mode)
+
+def end(request):
+    return render(request, 'experiment/end.html')
