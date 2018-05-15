ef66ac67e1be135ef00392d4e2ef68616fdcc398
dlwocks <frank_smic@hotmail.com>
2018-05-15 21:10:39 +0900

    base template added

--- experiment/models.py
+++ experiment/models.py
@@ -1,5 +1,17 @@
+'''
+Models of app `experiment`.
+Provides database model `Result`.
+'''
 from django.db import models
 
+# https://docs.djangoproject.com/en/2.0/ref/models/fields/
 class Result(models.Model):
-    res_date = models.DateTimeField('date when experiment is done')
-# Create your models here.
+    '''
+    Database scheme for experiment result.
+    '''
+    res_date = models.DateTimeField('Date when experiment is done')
+    timetext = models.TextField('Main data of experiment')
+    mode = models.BooleanField('True=mode1, False=mode2')
+    email = models.CharField('Email of the person', max_length=128)
+    is_receive_rep = models.BooleanField('Whether to receive report afterward')
+    is_reward = models.BooleanField('Whether to participate in reward chucheom.')
