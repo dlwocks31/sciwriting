51edd8e6ec9442f740a81737b764b04c26a54182
dlwocks <frank_smic@hotmail.com>
2018-05-17 15:17:49 +0900

    small experiment parameter+template fix

--- experiment/static/experiment/main.js
+++ experiment/static/experiment/main.js
@@ -53,8 +53,8 @@ function genchoice(i1, i2) {
 }
 
 function putquestion() {
-	i1 = Math.round(Math.random()*11+4);
-	i2 = Math.round(Math.random()*11+4);
+	i1 = Math.round(Math.random()*12+4);
+	i2 = Math.round(Math.random()*12+4);
 	ans = i1 * i2;
 	choices = genchoice(i1, i2);
 	data['questions'].push("{0} {1}".format(i1, i2));
