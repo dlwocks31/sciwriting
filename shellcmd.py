bcff1616eeb36115c06c896d7a2402b1032f0f1d
dlwocks <frank_smic@hotmail.com>
2018-05-20 17:16:32 +0900

    shellcmd update

--- shellcmd.py
+++ shellcmd.py
@@ -1,6 +1,7 @@
 from statistics import mean, median, stdev
 import json
 from experiment.models import User, Result
+from django.utils import timezone
 
 class Namespace:
     def __init__(self, **kwargs):
@@ -18,6 +19,26 @@ def get_all_firstresult(uid_min=0):
             data_notime.append({k:v for k,v in rawdata.items() if k != 'mode'})
     return data_hastime, data_notime
 
+FMT = '%Y-%m-%d %H:%M %z'
+BEFORE_MICHUT = "2018-05-20 17:00 +0900"
+TIME = BEFORE_MICHUT
+
+def getnow():
+    return timezone.now().strftime(FMT)
+
+def get_all_result_after(time=TIME):
+    time = datetime.strptime(time, FMT)
+    ls = list(r for r in Result.objects.all() if r.date > time)
+    data_hastime = []
+    data_notime = []
+    for r in ls:
+        rawdata = json.loads(r.data)
+        if rawdata['mode'] == 'hastime':
+            data_hastime.append({k:v for k,v in rawdata.items() if k != 'mode'})
+        elif rawdata['mode'] == 'notime':
+            data_notime.append({k:v for k,v in rawdata.items() if k != 'mode'})
+    return data_hastime, data_notime
+
 def analyze_average_final_time(data):
     avg = mean(int(d['answeredTime'][-1]) for d in data)
     med = median(int(d['answeredTime'][-1]) for d in data)
@@ -25,9 +46,9 @@ def analyze_average_final_time(data):
     return avg, med, std
 
 def analyze_average_intermediate_time(data):
-    avg = [mean(int(t) for d in data) for t in d['answeredTime']]
-    med = [median(int(d['answeredTime'][-1]) for d in data)]
-    std = stdev(int(d['answeredTime'][-1]) for d in data)
+    avg = [mean(data[i]['answeredTime'][j] for i in range(len(data[0]))) for j in range(len(data[0]['answeredTime']))]
+    med = [median(data[i]['answeredTime'][j] for i in range(len(data[0]))) for j in range(len(data[0]['answeredTime']))]
+    std = [stdev(data[i]['answeredTime'][j] for i in range(len(data[0]))) for j in range(len(data[0]['answeredTime']))]
     return avg, med, std
 
 def analyze_average_correct(data):
